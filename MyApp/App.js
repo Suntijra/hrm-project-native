e067bf4a607300fd7d1f8b36b9a9ad44a3fb9599
Sunti <saknsaza87@gmail.com>
2023-03-27 18:15:52 +0700

    update

--- MyApp/App.js
+++ MyApp/App.js
@@ -33,7 +33,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Home Screen test v1</Text>
     </View>
   );
 }
