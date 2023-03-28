938cd1177ae25a9ccedeaf99784cf7c5b2702b71
Sunti <saknsaza87@gmail.com>
2023-03-28 14:47:35 +0700

    login

--- MyApp/App.js
+++ MyApp/App.js
@@ -34,14 +34,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 function IntroScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', height: "100%" }}>
+      <View style={{ position: 'absolute', zIndex: 10 }}>
         <TouchableOpacity style={styles.introbtn} onPress={() => navigation.navigate('LoginScreen')}>
           <Image
             source={require('./src/assets/imgs/arrow-left-white.png')}
             style={{ width: 20, height: 16 }}
           />
         </TouchableOpacity>
+      </View>
+      <View style={styles.imgintro3}>
+        <Image style={styles.img3} source={require('./src/assets/imgs/bg_center.png')}></Image>
+        <Text style={styles.titile}>Lorem ipsum dolor sit amet consect.</Text>
+        <Text style={styles.para}>Lorem ipsum dolor sit amet consect.</Text>
+        <Text style={styles.para}>Lorem ipsum dolor sit amet consectconsect.</Text>
+      </View>
+      <View style={styles.imgintro1}>
+        <Image style={styles.img1} source={require('./src/assets/imgs/introbg1.png')}></Image>
+      </View>
+      <View style={styles.imgintro2}>
+        <Image style={styles.img2} source={require('./src/assets/imgs/introbg2.png')} ></Image>
       </View>
     </View>
   );
@@ -70,9 +82,57 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignContent: 'center',
     position: 'absolute',
-    bottom: -300,
-    right: -150
-
+    bottom: -330,
+    right: -180,
+  },
+  imgintro1: {
+    zIndex: 1,
+    width: 300,
+    height: 300
+  },
+  imgintro2: {
+    zIndex: 2,
+    width: 300,
+    height: 300
+  },
+  imgintro3: {
+    zIndex: 3,
+    width: 300,
+    height: 300
+  },
+  img1: {
+    height: 500,
+    resizeMode: "contain",
+    zIndex: 1,
+    bottom: -200,
+    left: -500
+  },
+  img2: {
+    height: 1000,
+    bottom: 700,
+    left: -600,
+    zIndex: 2,
+    resizeMode: "contain",
+  },
+  img3: {
+    height: 300,
+    bottom: -180,
+    left: -500,
+    zIndex: 5,
+    resizeMode: "contain",
+  },
+  titile: {
+    marginTop: 200,
+    textAlign: 'center',
+    fontWeight : "500",
+    fontSize : 20
+  },
+  para : {
+    textAlign: 'center',
+    fontWeight : "400",
+    fontSize : 16,
+    marginTop : 10,
+    width : '100%'
   }
 });
 
--- MyApp/src/screen/loginScreen.jsx
+++ MyApp/src/screen/loginScreen.jsx
@@ -22,7 +22,7 @@ function LoginScreen({ navigation }) {
                 <Text style={{ fontSize: 14, fontWeight: '400', marginTop: 10, marginBottom: 30 }} >Welcome to my app </Text>
 
                 <TextInput style={styles.input} placeholder="Email" />
-                <TextInput style={styles.input} placeholder="Password" />
+                <TextInput secureTextEntry={true} style={styles.input} placeholder="Password" />
                 <Text style={{ marginTop: 10, alignSelf: "flex-end", marginRight: 40, fontWeight: '600', color: '#6B6F80', textDecorationLine: 'underline' }}>Forget Password?</Text>
                 <TouchableOpacity style={{ justifyContent: 'center', alignItems: "center", borderWidth: 1, width: '90%', position: 'absolute', bottom: 50, height: 60, borderRadius: 16, borderColor: '#C7C9D9' }}>
                     <Text style={{ fontWeight: "700", fontSize: 14 }}>Sign in</Text>
